c98a18b20f14cace98d679f80f64bdb3924b7413
LevisOgembo <ogembolevis97@gmail.com>
2025-11-06 15:01:10 +0300

    New changes for users microservice

--- users-microservice/src/main.ts
+++ users-microservice/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
     transport: Transport.NATS,
     options:{
-      servers: [process.env.NATS_URL || 'nats://nats:4222'] //config
+      servers: [process.env.NATS_URL || 'nats://nats.service.local:4222'] //config
     }
   });
   await app.listen().then(()=>console.log('App is connected and listening via nats')
